692ebe76c1795fa71b1ffc951c38d4dcb59ad144
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-05-03 11:04:03 -0700

    Add xojs

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [
+    {
+      extends: ["xo-typescript"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
   rules: {
     "no-console": ["error", { allow: ["warn", "error"] }],
   },
-  extends: ["prettier"],
+  extends: ["xo", "prettier"],
 };
